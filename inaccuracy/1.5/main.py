e44d7feafee94c261c095a1e3cf3db21e80f4308
Gugush284 <gugush284@gmail.com>
2022-09-26 17:13:09 +0300

    Added comments and a graph

--- inaccuracy/1.5/main.py
+++ inaccuracy/1.5/main.py
@@ -1,35 +1,35 @@
-import numpy as np                  # импорт бибилиотеки numpy
-import matplotlib.pyplot as plt     # импорт модуля matplotlib.pyplot
+import numpy as np                                                          # Импорт бибилиотеки numpy
+import matplotlib.pyplot as plt                                             # Импорт модуля matplotlib.pyplot
 
-def gorner(x, coeff):
-    p = 1
+def gorner(x, coeff):                                                       # Функция подсчета методом Горнера
+    p = 1                                                                   # Присваеваем первый коэффициент полиному
 
-    for coeffi in coeff:
-        p = x*p + coeffi
+    for coeffi in coeff:                                                    # Алгоритм Горнера
+        p = x*p + coeffi                                                    
 
-    return p
+    return p                                                                    
 
 
 
-x = 1.92
-coeff = np.array([-18, 144, -672, 2016, -4032, 5376, -4608, 2304, -512])
-gorner_list = []
-eq_list = []
-x_list = []
+x = 1.92                                                                    # Начальное значение на отрезке
+coeff = np.array([-18, 144, -672, 2016, -4032, 5376, -4608, 2304, -512])    # Коэффициенты уравнения (x-2)^9, исключая первый
+gorner_list = []                                                            # Список, содержащий значения по Горнеру
+eq_list = []                                                                # Список значений, полученных решением (x-2)^9
+x_list = []                                                                 # Список значений отрезка [1.92, 2.08] с шагом 10^(-4)
 
-while x < 2.08:
-    p  = gorner(x, coeff)
-    eq = (x-2)**9
+while x < 2.08:                                                             # Пробегаем по отрезку
+    p = gorner(x, coeff)                                                    # Вызываем подсчет Горнером
+    eq = (x-2)**9                                                           # Считаем значение подстановкой
 
-    print(x, p, eq)
+    print(x, p, eq)                                                         # Вывод
 
-    eq_list.append(eq)
-    gorner_list.append(p)
-    x_list.append(x)
+    eq_list.append(eq)                                                      # Добавляем все посчитанные значения в списки
+    gorner_list.append(p)                                                   
+    x_list.append(x)                                                        
 
-    x += 10 ** -4
+    x += 10 ** -4                                                           # Делаем шаг 
 
-plt.figure(figsize=[12, 4]) 
-plt.plot(x_list, gorner_list)
-plt.plot(x_list, eq_list)
-plt.show()
+plt.figure(figsize=[12, 4])                                                 # Создаем полотно
+plt.plot(x_list, gorner_list)                                               # Построение графика для Горнера
+plt.plot(x_list, eq_list)                                                   # Построение точного графика
+plt.show()                                                                  # Вывод графика
